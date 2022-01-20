9b59e2d9b92a54d57bd2024011c436efd5211d28
lolmos <luis.olmos@cybrik.ai>
2022-01-20 23:27:03 +0100

    added sendgrid conf

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,16 +8,15 @@ module.exports = ({ env }) => ({
     },
   },
   email: {
-    provider: "sendmail",
-    providerOptions: {
-      dkim: {
-        privateKey: env("SENDMAIL_PRIVATE_KEY"),
-        keySelector: env("KEY_SELECTOR"), // the same as the one set in DNS txt record, use online dns lookup tools to be sure that is retreivable
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
       },
-    },
-    settings: {
-      defaultFrom: "notifications@canicacuadrada.com",
-      defaultReplyTo: "notifications@canicacuadrada.com",
+      settings: {
+        defaultFrom: "notifications@canicacuadrada.com",
+        defaultReplyTo: "notification@canicacuadrada.com",
+      },
     },
   },
 });
